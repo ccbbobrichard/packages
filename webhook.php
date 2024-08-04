32923fa1612e17b2865ac3769ee3b80747b52d7f
chencongbao <lhaiccb@126.com>
2024-08-04 09:42:59 +0800

    12
    12

--- webhook.php
+++ webhook.php
@@ -4,8 +4,5 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use ShopenGroup\SatisHook\ApplicationFactory;
 
-echo __DIR__ . '/logs';
-exit;
-
 $application = ApplicationFactory::createApplication(__DIR__ . '/config.yaml', __DIR__ . '/temp', __DIR__ . '/logs', $argc);
 $application->run();
